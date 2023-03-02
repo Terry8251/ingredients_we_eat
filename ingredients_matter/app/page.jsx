fb6373a5023751ea8f37297f492cda59acce965f
Terry8251 <terry8251@gmail.com>
2023-03-01 18:50:54 -0600

    intalling packages

--- ingredients_matter/app/page.jsx
+++ ingredients_matter/app/page.jsx
@@ -1,7 +1,15 @@
 export default function Home() {
   return (
     <main>
-      <h1>Hello next</h1>
+      <section className="h-screen bg-gradient-radial-at-t from-white to-black">
+        <h1 className="text-slate-300">Hello next</h1>
+      </section>
+      <section className="h-screen bg-gradient-radial-at-t from-black to-green-700">
+        <p className="text-white">
+          da;lvndap;gpainbv;adsjvna;nvpaqrevnba;jkd ;dcnv aiuhgipanbdv
+          aknmvmdnaoirnaernvaiodfnv'oniron
+        </p>
+      </section>
     </main>
   )
 }
--- ingredients_matter/tailwind.config.js
+++ ingredients_matter/tailwind.config.js
@@ -9,7 +9,19 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
-  },
-  plugins: [],
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(ellipse at center, var(--tw-gradient-stops))',
+        'gradient-radial-at-t': 'radial-gradient(55% 65% at 50% 0%, var(--tw-gradient-stops))',
+        'gradient-radial-at-b': 'radial-gradient(ellipse at bottom, var(--tw-gradient-stops))',
+        'gradient-radial-at-l': 'radial-gradient(ellipse at left, var(--tw-gradient-stops))',
+        'gradient-radial-at-r': 'radial-gradient(ellipse at right, var(--tw-gradient-stops))',
+        'gradient-radial-at-tl': 'radial-gradient(ellipse at top left, var(--tw-gradient-stops))',
+        'gradient-radial-at-tr': 'radial-gradient(ellipse at top right, var(--tw-gradient-stops))',
+        'gradient-radial-at-bl': 'radial-gradient(ellipse at bottom left, var(--tw-gradient-stops))',
+        'gradient-radial-at-br': 'radial-gradient(ellipse at bottom right, var(--tw-gradient-stops))',
+      },
+    },
+    plugins: [],
+  }
 }
